aaa2716e8557ef88eab4aed316c6a6fde196e45a
Omkar Dige <oyedige@gmail.com>
2020-07-07 17:26:11 +0530

    used bootstrap for styling

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_nav import Nav
+from flask_bootstrap import Bootstrap
 from config import Config
+from app.nav import topbar
+  
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+bootstrap = Bootstrap(app)
 
 from app import routes
