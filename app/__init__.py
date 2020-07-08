db128a62b7731a40b4d026c37c117f83d7690101
Omkar Dige <oyedige@gmail.com>
2020-07-08 16:05:33 +0530

    reformatted errors as flask blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,14 @@ from flask_nav import Nav
 from flask_bootstrap import Bootstrap
 from config import Config
 from app.nav import topbar
+from app.errors import bp as errors_bp
   
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+app.register_blueprint(errors_bp)
+
 bootstrap = Bootstrap(app)
 
-from app import routes, errors
+from app import routes
